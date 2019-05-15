89ecd5a9d2d5f088c8be6c9b8bf298faa7e1de75
Milos <milos.vujasinovic.98@gmail.com>
2019-05-15 12:46:34 +0200

    project added

---
+++ mvp/src/main/java/nightmarethreatreis/com/github/mvp/App.java
@@ -0,0 +1,13 @@
+package nightmarethreatreis.com.github.mvp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
